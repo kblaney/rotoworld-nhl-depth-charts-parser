a1988c5c59617d8248d13ab0a7a231f6db1e00d5
Kyle Blaney <blaney.kyle@gmail.com>
2013-01-23 16:42:34 -0500

    Put log messages inside private methods.

--- src/main/java/com/kblaney/rotoworld/parse/EntryPoint.java
+++ src/main/java/com/kblaney/rotoworld/parse/EntryPoint.java
@@ -9,23 +9,20 @@ public final class EntryPoint
 {
   public static void main(final String[] cmdLineArgs) throws IOException
   {
-    final String inputFileSpec = "C:/data/hockeypool/rotoWorldDepthCharts.txt";
-    System.out.println("Reading players from " + inputFileSpec + "...");
-    final List<Player> players = readPlayersFrom(inputFileSpec);
-
-    final String outputFileSpec = "C:/data/hockeypool/regularSeasonPlayers.csv.txt";
-    System.out.println("Writing players to " + outputFileSpec + "...");
-    writePlayers(players, outputFileSpec);
+    final List<Player> players = readPlayersFrom("C:/data/hockeypool/rotoWorldDepthCharts.txt");
+    writePlayers(players, "C:/data/hockeypool/regularSeasonPlayers.csv.txt");
     System.out.println("Done.");
   }
 
   private static List<Player> readPlayersFrom(final String fileSpec) throws IOException
   {
+    System.out.println("Reading players from " + fileSpec + "...");
     return new PlayersSupplier(new FileReader(fileSpec)).get();
   }
 
   private static void writePlayers(final List<Player> players, final String fileSpec) throws IOException
   {
+    System.out.println("Writing players to " + fileSpec + "...");
     new PlayersWriter(new FileWriter(fileSpec)).write(players);
   }
 }
