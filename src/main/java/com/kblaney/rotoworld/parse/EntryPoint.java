23062af6813b40f81d993bcd1152c89307065b61
Kyle Blaney <blaney.kyle@gmail.com>
2013-01-24 17:58:44 -0500

    Specify UTF-8 instead of relying on platform-specific encoding.

--- src/main/java/com/kblaney/rotoworld/parse/EntryPoint.java
+++ src/main/java/com/kblaney/rotoworld/parse/EntryPoint.java
@@ -1,12 +1,21 @@
 package com.kblaney.rotoworld.parse;
 
-import java.io.FileReader;
-import java.io.FileWriter;
+import com.google.common.base.Charsets;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.util.List;
 
 public final class EntryPoint
 {
+  private static final Charset CHARSET = Charsets.UTF_8;
+  private EntryPoint()
+  {
+  }
+
   public static void main(final String[] cmdLineArgs) throws IOException
   {
     final List<Player> players = readPlayersFrom("C:/data/hockeypool/rotoWorldDepthCharts.txt");
@@ -17,12 +26,12 @@ public final class EntryPoint
   private static List<Player> readPlayersFrom(final String fileSpec) throws IOException
   {
     System.out.println("Reading players from " + fileSpec + "...");
-    return new PlayersSupplier(new FileReader(fileSpec)).get();
+    return new PlayersSupplier(new InputStreamReader(new FileInputStream(fileSpec), CHARSET)).get();
   }
 
   private static void writePlayers(final List<Player> players, final String fileSpec) throws IOException
   {
     System.out.println("Writing players to " + fileSpec + "...");
-    new PlayersWriter(new FileWriter(fileSpec)).write(players);
+    new PlayersWriter(new OutputStreamWriter(new FileOutputStream(fileSpec), CHARSET)).write(players);
   }
 }
